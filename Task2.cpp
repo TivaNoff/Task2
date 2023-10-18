913c3b83b90974d939273a6c9b7f46ab9da1bc4f
TivaN <TivaN@DESKTOP-J6PVEAM>
2023-10-18 16:28:18 +0300

    Добавьте файлы проекта.

---
+++ Task2.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+struct Node {
+    int val;
+    Node* next;
+};
+
+Node* swapPairs(Node* head) {
+    if (head == nullptr || head->next == nullptr) {
+        return head;
+    }
+
+    Node* newHead = head->next;
+    head->next = swapPairs(newHead->next);
+    newHead->next = head;
+    return newHead;
+}
+
+int main() {
+    Node* head = new Node{ 1, new Node{2, new Node{3, new Node{4, nullptr}}} };
+    Node* newHead = swapPairs(head);
+    while (newHead != nullptr) {
+        cout << newHead->val << " ";
+        newHead = newHead->next;
+    }
+    cout << endl;
+    return 0;
+}
